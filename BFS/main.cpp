392033a512a843af693c9db921d3bc1afd5376ff
m3llad0 <dmelladooliveros@gmail.com>
2022-09-24 22:19:12 -0500

    main cpp

---
+++ BFS/main.cpp
@@ -0,0 +1,16 @@
+/*
+
+HW 09
+BFS algorithm implementation
+Diego Mellado A01655451
+Iwalani Amador A01732251
+Tonatiuh Reyes A01025459.
+
+*/
+#include "graph.hpp"
+
+int main(){
+
+   Graph g(4);
+
+}
